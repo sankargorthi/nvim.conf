ef900dee9ee463cdab045ce0c7a7fa04b333694a
Sankar Gorthi <sankar.gorthi@veeva.com>
2023-11-13 15:40:48 -0800

    ✨ Add more plugins that improve QoL

--- lua/sankar/plugins.lua
+++ lua/sankar/plugins.lua
@@ -17,13 +17,87 @@ return {
 
 	-- Git plugins
 	{ 'tpope/vim-fugitive' },
+
+	-- Undo
 	{ 'mbbill/undotree' },
 
 	-- blamer
 	{ 'APZelos/blamer.nvim' },
 
+	-- Detect tabstop and shiftwidth automatically
+	-- 'tpope/vim-sleuth',
+
+	{
+		-- Add indentation guides even on blank lines
+		'lukas-reineke/indent-blankline.nvim',
+		-- Enable `lukas-reineke/indent-blankline.nvim`
+		-- See `:help ibl`
+		main = 'ibl',
+		opts = {},
+	},
+
+	-- "gc" to comment visual regions/lines
+	{ 'numToStr/Comment.nvim', opts = {} },
+
 	-- UI Language plugins
-	{ 'norcalli/nvim-colorizer.lua' }
+	{ 'norcalli/nvim-colorizer.lua' },
+
+	-- { 'neovim/nvm-lspconfig' },
+
+	{
+		-- Adds git related signs to the gutter, as well as utilities for managing changes
+		'lewis6991/gitsigns.nvim',
+		opts = {
+			-- See `:help gitsigns.txt`
+			signs = {
+				add = { text = '+' },
+				change = { text = '~' },
+				delete = { text = '_' },
+				topdelete = { text = '‾' },
+				changedelete = { text = '~' },
+			},
+			on_attach = function(bufnr)
+				vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
+
+				-- don't override the built-in and fugitive keymaps
+				local gs = package.loaded.gitsigns
+				vim.keymap.set({ 'n', 'v' }, ']c', function()
+					if vim.wo.diff then
+						return ']c'
+					end
+					vim.schedule(function()
+						gs.next_hunk()
+					end)
+					return '<Ignore>'
+				end, { expr = true, buffer = bufnr, desc = 'Jump to next hunk' })
+				vim.keymap.set({ 'n', 'v' }, '[c', function()
+					if vim.wo.diff then
+						return '[c'
+					end
+					vim.schedule(function()
+						gs.prev_hunk()
+					end)
+					return '<Ignore>'
+				end, { expr = true, buffer = bufnr, desc = 'Jump to previous hunk' })
+			end,
+		},
+	},
+
+	{
+		-- Autocompletion
+		'hrsh7th/nvim-cmp',
+		dependencies = {
+			-- Snippet Engine & its associated nvim-cmp source
+			'L3MON4D3/LuaSnip',
+			'saadparwaiz1/cmp_luasnip',
+
+			-- Adds LSP completion capabilities
+			'hrsh7th/cmp-nvim-lsp',
+
+			-- Adds a number of user-friendly snippets
+			'rafamadriz/friendly-snippets',
+		},
+	},
 
 }
 
