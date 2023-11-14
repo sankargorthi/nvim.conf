a1388c7143277e28ce47fe158a4dac21b54d5ae2
Sankar Gorthi <sankar.gorthi@veeva.com>
2023-11-13 23:29:16 -0800

    ✨ Make the editor slightly prettier

--- init.lua
+++ init.lua
@@ -5,6 +5,12 @@ require("sankar")
 -- show line numbers
 vim.wo.number = true
 
+-- make editor prettier
+vim.o.scrolloff = 20
+vim.o.colorcolumn = "100"
+vim.o.signcolumn = "yes"
+
+-- nice tabs
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.softtabstop = 2
