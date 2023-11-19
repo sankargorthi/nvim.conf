eeedba318c7059f13206a7a6218a035f8e371c88
Sankar Gorthi <sankar.gorthi@veeva.com>
2023-11-18 19:22:14 -0800

    ✨ Configure lualine to work better with tmux
    - my tmux config already gives me the branch name. removed that
    - I care more about the full file name path

--- lua/sankar/plugins.lua
+++ lua/sankar/plugins.lua
@@ -35,6 +35,34 @@ return {
 				component_separators = '|',
 				section_separators = '',
 			},
+			sections = {
+				lualine_a = {'mode'},
+				lualine_b = {'diff', 'diagnostics'},
+				lualine_c = {
+					{
+						'filename',
+						path = 1,
+						shorting_target = 40,
+					}
+				},
+				lualine_x = {'encoding', 'fileformat', 'filetype'},
+				lualine_y = {'progress'},
+				lualine_z = {'location'}
+			},
+			inactive_sections = {
+				lualine_a = {'mode'},
+				lualine_b = {},
+				lualine_c = {
+					{
+						'filename',
+						path = 1,
+						shorting_target = 60
+					}
+				},
+				lualine_x = {'location'},
+				lualine_y = {},
+				lualine_z = {}
+			},
 		},
 	},
 
