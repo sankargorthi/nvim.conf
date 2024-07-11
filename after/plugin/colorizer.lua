386521276cc29990649cae5162180667cd695800
Sankar Gorthi <sankar.gorthi@veeva.com>
2024-07-10 22:20:00 -0700

    ❌ Turn off colorizer plugin for now

--- after/plugin/colorizer.lua
+++ after/plugin/colorizer.lua
@@ -1,12 +1,12 @@
-require 'colorizer'.setup({
-	css = {
-		hsl_fn = true
-	};
-	scss = {
-		hsl_fn = true
-	};
-	sass = {
-		hsl_fn = true
-	};
-})
+-- require 'colorizer'.setup({
+-- 	css = {
+	-- 	hsl_fn = true
+--	};
+--	scss = {
+--		hsl_fn = true
+--	};
+--	sass = {
+--		hsl_fn = true
+--	};
+--})
 -- All of these settings don't seem to work for colorizing hsl values
