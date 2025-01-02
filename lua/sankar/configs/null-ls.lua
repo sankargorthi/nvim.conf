63c9bee42f54bb101adfda52c12cef3f967bfa45
Sankar Gorthi <sankar.gorthi@veeva.com>
2025-01-01 16:19:08 -0800

    ✨ Only format javascript files

--- lua/sankar/configs/null-ls.lua
+++ lua/sankar/configs/null-ls.lua
@@ -2,26 +2,41 @@ local augrp = vim.api.nvim_create_augroup("LspFormatting", {})
 local null_ls = require 'null-ls'
 
 local opts = {
-  sources = {
+	sources = {
 		null_ls.builtins.formatting.prettierd,
 	},
-	on_attach = function (client, buffnr)
+	on_attach = function(client, bufnr)
 		if client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({
 				group = augrp,
-				buffer = buffnr,
+				buffer = bufnr,
 			})
+
+			-- Define a list of filetypes to check
+			local supported_filetypes = {
+				"javascript",
+				"javascriptreact",
+				"typescript",
+				"typescriptreact",
+			}
+
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				group = augrp,
-				buffer = buffnr,
-				callback = function ()
-					vim.lsp.buf.format({ bufnr = buffnr })
-					local params = {
-						command = '_typescript.organizeImports',
-						arguments = {vim.api.nvim_buf_get_name(0)},
-					}
-					vim.lsp.buf.execute_command(params)
-				end
+				buffer = bufnr, -- Scoped to this specific buffer
+				callback = function()
+					local filetype = vim.bo[bufnr].filetype
+					if vim.tbl_contains(supported_filetypes, filetype) then
+						-- Format the buffer
+						vim.lsp.buf.format({ bufnr = bufnr })
+
+						-- Organize imports
+						local params = {
+							command = '_typescript.organizeImports',
+							arguments = { vim.api.nvim_buf_get_name(bufnr) },
+						}
+						vim.lsp.buf.execute_command(params)
+					end
+				end,
 			})
 		end
 	end
