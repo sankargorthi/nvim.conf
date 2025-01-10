bcc4ac3d26bda9647f7482c4b392843ad9abe3d8
Sankar Gorthi <sankar.gorthi@veeva.com>
2025-01-10 07:56:49 -0800

    ⌨ Keymaps for rotating windows

--- lua/sankar/remap.lua
+++ lua/sankar/remap.lua
@@ -1,2 +1,4 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>wd", "<C-w>t<C-w>K", { desc = 'Rotate windows down' })
+vim.keymap.set("n", "<leader>wr", "<C-w>t<C-w>H", { desc = 'Rotate windows right' })
