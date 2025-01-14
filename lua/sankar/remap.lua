3aef70b7acda02639868af80823966b21df78658
Sankar Gorthi <sankar.gorthi@veeva.com>
2025-01-13 20:16:45 -0800

    ⌨️ Add keymap to refresh the config

--- lua/sankar/remap.lua
+++ lua/sankar/remap.lua
@@ -2,3 +2,8 @@ vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>wd", "<C-w>t<C-w>K", { desc = 'Rotate windows down' })
 vim.keymap.set("n", "<leader>wr", "<C-w>t<C-w>H", { desc = 'Rotate windows right' })
+vim.keymap.set('n', '<leader>rc', function()
+	dofile(vim.env.MYVIMRC)
+	print("Config reloaded!")
+end, { desc = "Refresh Neovim configuration" })
+
