0a3a896b33407c6ef232a9faf02639eeef2cacc7
Sankar Gorthi <sankar.gorthi@veeva.com>
2025-01-13 22:25:05 -0800

    ✨ Update dependencies and add custom lualine config

---
+++ lua/sankar/lualine.lua
@@ -0,0 +1,38 @@
+local M = {
+	options = {
+		icons_enabled = true,
+		component_separators = '|',
+		theme = 'gruvbox-material',
+		section_separators = '',
+	},
+	sections = {
+		lualine_a = { 'mode' },
+		lualine_b = { 'branch', 'diagnostics' },
+		lualine_c = {
+			{
+				'filename',
+				path = 1,
+				shorting_target = 40,
+			}
+		},
+		lualine_x = { 'diff', 'encoding', 'fileformat', 'filetype' },
+		lualine_y = { 'progress' },
+		lualine_z = { 'location' }
+	},
+	inactive_sections = {
+		lualine_a = { 'mode' },
+		lualine_b = {},
+		lualine_c = {
+			{
+				'filename',
+				path = 1,
+				shorting_target = 60
+			}
+		},
+		lualine_x = { 'location' },
+		lualine_y = {},
+		lualine_z = {}
+	},
+}
+
+return M
