30122924abff6249e11ae3f0e26cf794bc7b8e13
Sankar Gorthi <sankar.gorthi@veeva.com>
2025-01-15 23:15:02 -0800

    ✨ Scope gitlab MR search to relevant ones for now

--- lua/sankar/gitlab.lua
+++ lua/sankar/gitlab.lua
@@ -1,4 +1,19 @@
-require("gitlab").setup()
+local Gitlab = require "gitlab"
+
+-- Initialize gitlab
+Gitlab.setup()
+
+-- Let's remap some bindings
+vim.keymap.set("n", "glc", function()
+		Gitlab.choose_merge_request({ reviewer_username = 'sankar.gorthi', open_reviewer = true })
+	end,
+	{ desc = "Show all of sankar's review requests" });
+
+vim.keymap.set("n", "glM", function()
+	vim.print("Don't merge from here. Use a better way")
+end,
+	{ desc = "Stop me from accidentally merging from here" });
+
 
 -- vim.keymap.set('n', 'glM', function()
 -- 	vim.ui.select({
